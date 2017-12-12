76382f78028dc7456c2e196db4afd7ccab3a7bed
Kolya Siryk <nikolay.siryk@bigdropinc.com>
2017-12-12 16:02:01 +0200

    update behavior

--- behaviors/DateBehavior.php
+++ behaviors/DateBehavior.php
@@ -3,6 +3,7 @@
 namespace kolyasiryk\yii2behavior\behaviors;
 
 use yii\base\Behavior;
+use yii\base\Exception;
 use yii\base\InvalidParamException;
 use yii\db\BaseActiveRecord;
 use yii\helpers\ArrayHelper;
@@ -16,45 +17,35 @@ use yii\i18n\Formatter;
  */
 class DateBehavior extends Behavior
 {
-    /**
-     * @var array
-     */
+    const EVENT_SET = 'eventSet';
+    const EVENT_GET = 'eventGet';
+
+    /** @var array */
     public $attributes = [];
 
-    /**
-     * @var Formatter
-     */
+    /** @var Formatter */
     public $formatter;
 
-    /**
-     * @var string
-     */
-    public $dateSaveFormat = 'php:Y-m-d';
+    /** @var string */
+    public $dateSaveFormat = 'Y-m-d';
 
-    /**
-     * @var string
-     */
-    public $datetimeSaveFormat = 'php:Y-m-d H:i:s';
+    /** @var string */
+    public $datetimeSaveFormat = 'Y-m-d H:i:s';
 
-    /**
-     * @var string
-     */
-    public $timeSaveFormat = 'php:H:i:s';
+    /** @var string */
+    public $timeSaveFormat = 'H:i:s';
 
-    /**
-     * @var string
-     */
-    public $dateDisplayFormat = 'php:Y-m-d';
+    /** @var string */
+    public $dateDisplayFormat = 'Y-m-d';
 
-    /**
-     * @var string
-     */
-    public $datetimeDisplayFormat = 'php:Y-m-d H:i:s';
+    /** @var string */
+    public $datetimeDisplayFormat = 'Y-m-d H:i:s';
 
-    /**
-     * @var string
-     */
-    public $timeDisplayFormat = 'php:H:i:s';
+    /** @var string */
+    public $timeDisplayFormat = 'H:i:s';
+
+    /** @var string */
+    public $userTimezone;
 
     /**
      * @inheritdoc
@@ -66,6 +57,8 @@ class DateBehavior extends Behavior
         } elseif (is_array($this->formatter)) {
             $this->formatter = \Yii::createObject($this->formatter);
         }
+
+        $this->userTimezone = $this->userTimezone ?? \Yii::$app->params['userTimezone'] ?? 'GMT';
     }
 
     /**
@@ -87,7 +80,7 @@ class DateBehavior extends Behavior
      */
     public function onBeforeSave()
     {
-        $this->updateAttributes($this->saveFormats());
+        $this->updateAttributes($this->saveFormats(), self::EVENT_SET);
     }
 
     /**
@@ -97,15 +90,17 @@ class DateBehavior extends Behavior
      */
     public function onAfterFind()
     {
-        $this->updateAttributes($this->displayFormats());
+        $this->updateAttributes($this->displayFormats(), self::EVENT_GET);
     }
 
     /**
      * Update attributes
      *
      * @param array $formats
+     * @param string $event
+     * @throws InvalidParamException|Exception
      */
-    protected function updateAttributes($formats)
+    protected function updateAttributes($formats, $event)
     {
         foreach ($this->attributes as $attribute => $format) {
             if ($value = $this->owner->getAttribute($attribute)) {
@@ -113,11 +108,26 @@ class DateBehavior extends Behavior
                     throw new InvalidParamException('$format has incorrect value');
                 }
 
-                if ($dateFormat === 'U') {
-                    $value = (int) $this->formatter->asTimestamp($value);
-                } else {
-                    $value = $this->formatter->format($value, [$format, $dateFormat]);
+                $userTimezone = new \DateTimeZone($this->userTimezone);
+                $gmtTimezone = new \DateTimeZone('UTC');
+                $myDateTime = new \DateTime(is_int($value) ? date('Y-m-d H:i:s', $value) : $value, $gmtTimezone);
+
+                switch ($event) {
+                    case self::EVENT_GET:
+                        $offset = $userTimezone->getOffset($myDateTime);
+                        break;
+                    case self::EVENT_SET:
+                        $offset = -1 * $userTimezone->getOffset($myDateTime);
+                        break;
+                    default:
+                        throw new Exception('Unknown event');
                 }
+
+                $myInterval = \DateInterval::createFromDateString((string)$offset . 'seconds');
+                $myDateTime->add($myInterval);
+                $value = $myDateTime->format($dateFormat);
+
+                $value = $dateFormat === 'U' ? (int) $value : $value;
 
                 $this->owner->setAttribute($attribute, $value);
             }
